97c6fcb34012b822a50cf46cca0d0e9353b546fc
TheProgersTeam <68988841+TheProgersTeam@users.noreply.github.com>
2021-01-19 22:51:14 +0300

    Update Requests.py

--- Snake Game/Requests.py
+++ Snake Game/Requests.py
@@ -3,16 +3,16 @@ import requests
 
 class Requests:
     # Отправляем результат игрока и его ник
-    def Post(player, score):    
+    def Post(player, score):
         url = "http://127.0.0.1:8000/api/add/"
         payload = {"data": {"player": player, "score": score}}
         headers = {'Content-Type': 'application/json'}
         requests.request("POST", url, headers=headers, json=payload)
+
     # Получаем список лучших игроков
     def Get():
         url = "http://127.0.0.1:8000/api/get/"
-        payload={}
+        payload = {}
         headers = {}
         response = requests.request("GET", url, headers=headers, data=payload)
         return response.json()
-
